42c00e0d23734fa9f3fe521e4bacf5c5baaf71ed
VALETOR <VALETOR@DESKTOP-5SFNMNU>
2026-05-23 23:44:34 -0500

    ACTUALIZACIÓN DE VALETOR

--- src/main/java/com/bicicletas/views/Login.java
+++ src/main/java/com/bicicletas/views/Login.java
@@ -30,69 +30,136 @@ public class Login extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        RolAdmin = new javax.swing.JButton();
+        RolStudent = new javax.swing.JButton();
+        EligeRol = new javax.swing.JButton();
+        InfoApp = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setFont(new java.awt.Font("Ancizar Serif", 0, 24)); // NOI18N
-        jButton1.setLabel("Administrador");
-        jButton1.addActionListener(this::jButton1ActionPerformed);
+        jPanel1.setBackground(new java.awt.Color(255, 153, 153));
 
-        jButton2.setFont(new java.awt.Font("Ancizar Serif", 0, 24)); // NOI18N
-        jButton2.setLabel("Estudiante");
-        jButton2.addActionListener(this::jButton2ActionPerformed);
+        RolAdmin.setBackground(new java.awt.Color(153, 0, 51));
+        RolAdmin.setFont(new java.awt.Font("Ancizar Serif", 0, 24)); // NOI18N
+        RolAdmin.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        RolAdmin.setBorderPainted(false);
+        RolAdmin.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        RolAdmin.setLabel("Administrador");
+        RolAdmin.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                RolAdminMouseClicked(evt);
+            }
+        });
+        RolAdmin.addActionListener(this::RolAdminActionPerformed);
 
-        jLabel1.setFont(new java.awt.Font("Ancizar Serif Black", 0, 48)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("¿Cuál es tu rol?");
+        RolStudent.setBackground(new java.awt.Color(153, 0, 51));
+        RolStudent.setFont(new java.awt.Font("Ancizar Serif", 0, 24)); // NOI18N
+        RolStudent.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        RolStudent.setBorderPainted(false);
+        RolStudent.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        RolStudent.setLabel("Estudiante");
+        RolStudent.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                RolStudentMouseClicked(evt);
+            }
+        });
+        RolStudent.addActionListener(this::RolStudentActionPerformed);
+
+        EligeRol.setBackground(new java.awt.Color(153, 0, 51));
+        EligeRol.setFont(new java.awt.Font("Ancizar Serif", 1, 48)); // NOI18N
+        EligeRol.setText("Elige tu rol");
+        EligeRol.setBorder(null);
+        EligeRol.setBorderPainted(false);
+        EligeRol.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        EligeRol.setFocusPainted(false);
+        EligeRol.addActionListener(this::EligeRolActionPerformed);
+
+        InfoApp.setBackground(new java.awt.Color(153, 0, 51));
+        InfoApp.setFont(new java.awt.Font("Ancizar Sans Thin", 0, 18)); // NOI18N
+        InfoApp.setText("Bicirrun");
+        InfoApp.setBorder(null);
+        InfoApp.setBorderPainted(false);
+        InfoApp.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        InfoApp.addActionListener(this::InfoAppActionPerformed);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(100, 100, 100)
-                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
-                .addGap(100, 100, 100)
-                .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
-                .addGap(118, 118, 118))
-            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(148, 148, 148)
+                .addComponent(RolAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 269, Short.MAX_VALUE)
+                .addComponent(RolStudent, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(150, 150, 150))
+            .addComponent(EligeRol, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(InfoApp, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(56, 56, 56)
+                .addGap(16, 16, 16)
+                .addComponent(EligeRol, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(150, Short.MAX_VALUE))
+                    .addComponent(RolStudent, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(RolAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 122, Short.MAX_VALUE)
+                .addComponent(InfoApp, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(57, 57, 57)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(390, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void RolStudentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RolStudentActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_RolStudentActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void RolAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RolAdminActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_RolAdminActionPerformed
+
+    private void EligeRolActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EligeRolActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_EligeRolActionPerformed
+
+    private void RolAdminMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RolAdminMouseClicked
+        // TODO add your handling code here:
+        RegistroAdmin registroAdmin=new RegistroAdmin();
+        registroAdmin.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_RolAdminMouseClicked
+
+    private void InfoAppActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InfoAppActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_InfoAppActionPerformed
+
+    private void RolStudentMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RolStudentMouseClicked
+
+        // TODO add your handling code here:
+        RegistroEstudiante registroEstudiante=new RegistroEstudiante();
+        registroEstudiante.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_RolStudentMouseClicked
 
     /**
      * @param args the command line arguments
@@ -120,9 +187,10 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton EligeRol;
+    private javax.swing.JButton InfoApp;
+    private javax.swing.JButton RolAdmin;
+    private javax.swing.JButton RolStudent;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
