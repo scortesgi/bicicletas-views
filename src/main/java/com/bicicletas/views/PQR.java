f64d0450a0ae4271b79d5080544b7ac66b71269e
vatorresra <vatorresra@unal.edu.co>
2026-05-31 13:16:27 -0500

    Comentario

--- src/main/java/com/bicicletas/views/PQR.java
+++ src/main/java/com/bicicletas/views/PQR.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
+
+//Solo para aparecer como contribuidora 
 package com.bicicletas.views;
 
 import java.awt.Color;
